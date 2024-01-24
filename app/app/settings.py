5d9f37f16c2c16fff0212049a5a6e909860d804a
Haenes <Physeex@gmail.com>
2024-01-25 01:05:16 +1000

    Fix: fix prev wrong commit

--- app/app/settings.py
+++ app/app/settings.py
@@ -107,11 +107,15 @@ EMAIL_ADMIN = os.environ.get('EMAIL_ADMIN')
 
 
 DATABASES = {
-    'default': {
-      'ENGINE': 'django.db.backends.sqlite3',
-      'NAME': BASE_DIR / 'db.sqlite3',
-    }
- }
+	'default': {
+    	'ENGINE': 'django.db.backends.mysql',
+    	'NAME': 'bugtracker',
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'PORT': os.environ.get('DATABASE_PORT'),
+        'HOST': os.environ.get('DATABASE_HOST'),
+	}
+}
 
 
 AUTH_PASSWORD_VALIDATORS = [
