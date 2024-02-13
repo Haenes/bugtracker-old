6b3d6b8552aa8688ba22282d35649f1c9fd958cd
Haenes <Physeex@gmail.com>
2024-02-14 05:50:13 +1000

    Feat: add waiting for the db to be fully enabled before connecting to it + add a script to createsuperuser for Django

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,29 @@
 
 cd app
 
-echo "----- Collect static files ------ " 
+echo "----------- Collect static files -----------" 
 python manage.py collectstatic --no-input
 
-echo "-----------Apply migration--------- "
+echo "----------- Apply migration -----------"
 python manage.py makemigrations && python manage.py migrate --no-input
 
-echo "-----------Run --------- "
+
+echo "----------- Create superuser -----------" 
+script="
+from django.contrib.auth.models import User;
+
+username = 'test';
+password = 'Test123#';
+email = 'test@test.test';
+
+if not User.objects.filter(username=username):
+    User.objects.create_superuser(username, email, password);
+    print('Superuser created.');
+else:
+    print('Superuser already exists.');
+"
+printf "$script" | python manage.py shell
+
+
+echo "----------- Run -----------"
 gunicorn app.wsgi:application --bind 0.0.0.0:8000
