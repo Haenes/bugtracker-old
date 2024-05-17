0aa48032f1aa153c9b28aad99ebe6b5db15f0efc
Berserk <Physeex@gmail.com>
2024-05-17 11:27:43 +1000

    Refactor: make some minor changes to simplify the app's deploy

--- app/app/settings.py
+++ app/app/settings.py
@@ -10,9 +10,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('DJANGO_SECRET')
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['0.0.0.0']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
